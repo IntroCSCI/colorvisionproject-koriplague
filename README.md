433e60673641ee55225c895f14c2f6487be080d3
Tobias Huybers <tdhuybers@mail.csuchico.edu>
2021-03-12 07:25:55 +0000

    Marked down in README.md

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# !!!Replace with your unique project name!!!
+# To Be Visable or Not
 
 ## Description
 
-!!!Replace with a description of what your program does!!!
+03/10/21: The program asks what values of RGB and if the values are in a specific bound the program will state what kind of color it is.
 
 ### v0.2 Updates
 
@@ -15,7 +15,7 @@
 
 ## Developer
 
-!!!Replace with your name!!!
+Tobias Huybers
 
 ## Example
 
@@ -29,30 +29,42 @@ g++ --std=c++11 *.cpp -o cvp
 Here is an example of the program running:
 
 ```
-!!!Replace with an example of the program running with user interaction!!!
+Please enter the values of Red, Green , and Blue
+
+Enter value for red (0-255): 55
+
+Enter value for green (0-255): 255
+
+Enter value for Blue (0-255): 100
+
+Red: 55
+Green: 255
+Blue: 100
+
+The color values make green-ish
 ```
 
 ## C++ Guide
 
 ### Variables and Data Types
 
-!!!Replace with a summary and examples of how multiple variables have been used with a variety of data types and why they have been used effectively and appropriately!!!
+3/10/21: The only variables as of right now are only integer variables to hold the number value of what the user plugged in, however it also has integer values of the red, green, and blue values subtracted by each other to make upper and lower bounds for deciding what color the input is.
 
 ### Console Input and Output
 
-!!!Replace with a summary and examples of how input and output have been used effectively and appropriately!!!
+3/10/21: The only console input the user plugs in is the values for either red, green, or blue. However the console outputs the promt for these values, the values you put in, and if the user's values are in the upper and lower bounds a special message that states about what the color is.
 
 ### Decisions
 
-!!!Replace with a summary and examples of how multiple decision constructs have been used effectively and appropriately!!!
+3/10/21: There are multiple decisions this code has to make, like first if the value inputed by the user is in the appropriate bounds and restates the prompt with an error message if they didn't input the right value. The code also makes the decision to add the special message if the values fit the strict statements to say the message. 
 
 ### Iteration
 
-!!!Replace with a summary and examples of how iteration has been used effectively and appropriately!!!
+3/10/21: The Iteration added to the code is when the user plugs in a value for Red, Green, or Blue will loop if they input a value below or above the bounds placed for a RGB value; However the loop will break if they plug in an appropriate values.
 
 ### File Input and Output
 
-!!!Replace with a summary and examples of how input and/or output of files have been used effectively and appropriately!!!
+3/10/21: There have been no file input or outputs as of this date.
 
 ### Arrays/Vectors
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,137 @@
-//Author: PUT YOUR NAME HERE
+//Author: Tobias Huybers
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
 
+int cred = 0;
+int cgreen = 0;
+int cblue = 0;
+
+cout << "Please enter the values of Red, Green , and Blue" << endl;
+
+//Finding Color Red
+cout << endl;
+cout << "Enter value for red (0-255): ";
+cin >> cred;
+
+while (cred <= 0 || cred >= 0){
+  if (cred < 0 || cred > 255){
+    cout << "ERROR: value not between 0-255" << endl;
+    cout << endl;
+    cout << "Enter value for red (0-255): ";
+    cin >> cred;
+  } else {
+    break;
+  }
+}
+
+//Finding Color Green
+cout << endl;
+cout << "Enter value for green (0-255): ";
+cin >> cgreen;
+
+while (cgreen <= 0 || cgreen >= 0){
+  if (cgreen < 0 || cgreen > 255){
+    cout << "ERROR: value not between 0-255" << endl;
+    cout << endl;
+    cout << "Enter value for green (0-255): ";
+    cin >> cgreen;
+  } else {
+    break;
+  }
+}
+
+//Finding Color Blue
+cout << endl;
+cout << "Enter value for Blue (0-255): ";
+cin >> cblue;
+
+while (cblue <= 0 || cblue >= 0){
+  if (cblue < 0 || cblue > 255){
+    cout << "ERROR: value not between 0-255" << endl;
+    cout << endl;
+    cout << "Enter value for blue (0-255): ";
+    cin >> cblue;
+  } else {
+    break;
+  }
+}
+
+cout << endl;
+cout << "Red: " << cred << endl;
+cout << "Green: " << cgreen << endl;
+cout << "Blue: " << cblue << endl;
+
+int crg = cred - cgreen;
+int crb = cred - cblue;
+int cgr = cgreen - cred;
+int cgb = cgreen - cblue;
+int cbr = cblue - cred;
+int cbg = cblue - cgreen;
+
+//If they made Grey Colors
+if (cred == cgreen && cgreen == cblue && cred == 255){
+  cout << endl;
+  cout << "The color values make white" << endl;
+}
+else if (cred == cgreen && cgreen == cblue && cred > 169 && cred < 255){
+  cout << endl;
+  cout << "The color values make light grey" << endl;
+}
+else if (cred == cgreen && cgreen == cblue && cred > 84 && cred <= 169){
+  cout << endl;
+  cout << "The color values make grey" << endl;
+}
+else if (cred == cgreen && cgreen == cblue && cred > 0 && cred <= 84){
+  cout << endl;
+  cout << "The color values make dark grey" << endl;
+}
+else if (cred == cgreen && cgreen == cblue && cred == 0){
+  cout << endl;
+  cout << "The color values make black" << endl;
+}
+
+//If they make Red
+if (cgreen == cblue && cgreen < cred){
+    cout << endl;
+    cout << "The color values make red" << endl;
+}
+else if (cgb > 0 && cgb <= 50 && cgreen < cred){
+    cout << endl;
+    cout << "The color values make red-ish" << endl;
+}
+else if (cbg > 0 && cbg <= 50 && cblue < cred){
+    cout << endl;
+    cout << "The color values make red-ish" << endl;
+}
+
+//If they make Green
+if (cred == cblue && cred < cgreen){
+    cout << endl;
+    cout << "The color values make green" << endl;
+}
+else if (crb > 0 && crb <= 50 && cred < cgreen){
+    cout << endl;
+    cout << "The color values make green-ish" << endl;
+}
+else if (cbr > 0 && cbr <= 50 && cblue < cgreen){
+    cout << endl;
+    cout << "The color values make green-ish" << endl;
+}
+
+//If they make Blue
+if (cred == cgreen && cred < cblue){
+    cout << endl;
+    cout << "The color values make blue" << endl;
+}
+else if (crg > 0 && crg <= 50 && cred < cblue){
+    cout << endl;
+    cout << "The color values make blue-ish" << endl;
+}
+else if (cgr > 0 && cgr <= 50 && cgreen < cblue){
+    cout << endl;
+    cout << "The color values make blue-ish" << endl;
+}
   return 0;
 }
