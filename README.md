a1ee0a307dded325a94102f7671be2120e427916
Tobias Huybers <>
2021-04-22 00:33:26 +0000

    The program has slimmed the main code, while also adding the ability for the coder to make 5 other colors and save them to 'RGBvalues.txt'.

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## Description
 
-03/10/21: The program asks what values of RGB and if the values are in a specific bound the program will state what kind of color it is.
+04/21/21: The program asks what values of RGB and if the values are in a specific bound the program will state what kind of color it is, afterwards the code will state 5 more different RGB color values and save both the 5 new and inputed values into 'RGBvalues.txt'.
 
 ### v0.2 Updates
 
-*Coming soon*
+The program has slimmed the main code, while also adding the ability for the coder to make 5 other colors and save them to 'RGBvalues.txt'.
 
 ### v1.0 Updates
 
@@ -44,15 +44,31 @@ Blue: 100
 
 The color values make green-ish
 
-Do you want to save to a File 'RGBvalues.md'? (Y or N)
-y
+Here are 5 other colors:
+251, 141, 255
+46, 242, 90
+93, 27, 13
+201, 94, 99
+74, 154, 49
 
-File Saved
+Do you want to save to a File 'RGBvalues.md'? (Y or N)
+Y
+
+!File Saved!
 
 File 'RGBvalues.txt' says:
+
+The Values You Picked:
 R: 55
 G: 255
 B: 100
+
+The 5 other Colors:
+251, 141, 255
+46, 242, 90
+93, 27, 13
+201, 94, 99
+74, 154, 49
 
 ```
 
@@ -64,27 +80,27 @@ B: 100
 
 ### Console Input and Output
 
-3/10/21: The only console input the user plugs in is the values for either red, green, or blue. However the console outputs the promt for these values, the values you put in, and if the user's values are in the upper and lower bounds a special message that states about what the color is.
+03/10/21: The only console input the user plugs in is the values for either red, green, or blue. However the console outputs the promt for these values, the values you put in, and if the user's values are in the upper and lower bounds a special message that states about what the color is.
 
 ### Decisions
 
-3/10/21: There are multiple decisions this code has to make, like first if the value inputed by the user is in the appropriate bounds and restates the prompt with an error message if they didn't input the right value. The code also makes the decision to add the special message if the values fit the strict statements to say the message. 
+03/10/21: There are multiple decisions this code has to make, like first if the value inputed by the user is in the appropriate bounds and restates the prompt with an error message if they didn't input the right value. The code also makes the decision to add the special message if the values fit the strict statements to say the message. 
 
 ### Iteration
 
-3/10/21: The Iteration added to the code is when the user plugs in a value for Red, Green, or Blue will loop if they input a value below or above the bounds placed for a RGB value; However the loop will break if they plug in an appropriate values.
+03/10/21: The Iteration added to the code is when the user plugs in a value for Red, Green, or Blue will loop if they input a value below or above the bounds placed for a RGB value; However the loop will break if they plug in an appropriate values.
 
 ### File Input and Output
 
-3/12/21: After everything is finished, the program will ask if you want to save the values of Red, Blue and Green to a file. Answering yes will implement the values into it, saying no will show that the file is empty.
+03/12/21: After everything is finished, the program will ask if you want to save the values of Red, Blue and Green to a file. Answering yes will implement the values into it, saying no will show that the file is empty.
 
 ### Arrays/Vectors
 
-*Coming in version 0.2*
+04/21/21: There are a couple instances where I used Vectors in the code. I mostly used them to help store the values of the 5 other colors. So the code generates 50 different values between 0 and 255 and it stores the numbers into a Vector. Then I have the code pick 5 different values from the 50 and store it into 1 of 3 Vectors for Red, Green, and Blue.
 
 ### Functions
 
-*Coming in version 0.2*
+04/21/21: There are 2 functions that I have created for this code, one was to slim down the main code. It's a void return type function that check if the user put in a value between 0 and 255. The other is a integer return type function that picks one number form a list of 50 values and convert it from a vector value to a plain integer.
 
 ### Classes
 
--- main.cpp
+++ main.cpp
@@ -1,17 +1,27 @@
 //Author: Tobias Huybers
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <cstdlib>
 using namespace std;
+
+//Prototypes
+void checkValues( int & );
+int randomColor(vector <int>);
 
 int main() {
 
+//Variables
 int cred = 0;
 int cgreen = 0;
 int cblue = 0;
 fstream reader;
 char letter;
-char letter2;
-string line = "";
+string line;
+vector <int> list;
+vector <int> redlist;
+vector <int> greenlist;
+vector <int> bluelist;
 
 cout << "Please enter the values of Red, Green , and Blue" << endl;
 
@@ -19,50 +29,21 @@ cout << "Please enter the values of Red, Green , and Blue" << endl;
 cout << endl;
 cout << "Enter value for red (0-255): ";
 cin >> cred;
-
-while (cred <= 0 || cred >= 0){
-  if (cred < 0 || cred > 255){
-    cout << "ERROR: value not between 0-255" << endl;
-    cout << endl;
-    cout << "Enter value for red (0-255): ";
-    cin >> cred;
-  } else {
-    break;
-  }
-}
+checkValues(cred);
 
 //Finding Color Green
 cout << endl;
 cout << "Enter value for green (0-255): ";
 cin >> cgreen;
-
-while (cgreen <= 0 || cgreen >= 0){
-  if (cgreen < 0 || cgreen > 255){
-    cout << "ERROR: value not between 0-255" << endl;
-    cout << endl;
-    cout << "Enter value for green (0-255): ";
-    cin >> cgreen;
-  } else {
-    break;
-  }
-}
+checkValues(cgreen);
 
 //Finding Color Blue
 cout << endl;
 cout << "Enter value for Blue (0-255): ";
 cin >> cblue;
+checkValues(cblue);
 
-while (cblue <= 0 || cblue >= 0){
-  if (cblue < 0 || cblue > 255){
-    cout << "ERROR: value not between 0-255" << endl;
-    cout << endl;
-    cout << "Enter value for blue (0-255): ";
-    cin >> cblue;
-  } else {
-    break;
-  }
-}
-
+//Displays the Inputed Values
 cout << endl;
 cout << "Red: " << cred << endl;
 cout << "Green: " << cgreen << endl;
@@ -78,84 +59,115 @@ int cbg = cblue - cgreen;
 //If they made Grey Colors
 if (cred == cgreen && cgreen == cblue && cred == 255){
   cout << endl;
-  cout << "The color values make white" << endl;
+  cout << "The values you chose make white" << endl;
 }
 else if (cred == cgreen && cgreen == cblue && cred > 169 && cred < 255){
   cout << endl;
-  cout << "The color values make light grey" << endl;
+  cout << "The values you chose make light grey" << endl;
 }
 else if (cred == cgreen && cgreen == cblue && cred > 84 && cred <= 169){
   cout << endl;
-  cout << "The color values make grey" << endl;
+  cout << "The values you chose make grey" << endl;
 }
 else if (cred == cgreen && cgreen == cblue && cred > 0 && cred <= 84){
   cout << endl;
-  cout << "The color values make dark grey" << endl;
+  cout << "The values you chose make dark grey" << endl;
 }
 else if (cred == cgreen && cgreen == cblue && cred == 0){
   cout << endl;
-  cout << "The color values make black" << endl;
+  cout << "The values you chose make black" << endl;
 }
 
 //If they make Red
 if (cgreen == cblue && cgreen < cred){
     cout << endl;
-    cout << "The color values make red" << endl;
+    cout << "The values you chose make red" << endl;
 }
 else if (cgb > 0 && cgb <= 50 && cgreen < cred){
     cout << endl;
-    cout << "The color values make red-ish" << endl;
+    cout << "The values you chose make red-ish" << endl;
 }
 else if (cbg > 0 && cbg <= 50 && cblue < cred){
     cout << endl;
-    cout << "The color values make red-ish" << endl;
+    cout << "The values you chose make red-ish" << endl;
 }
 
 //If they make Green
 if (cred == cblue && cred < cgreen){
     cout << endl;
-    cout << "The color values make green" << endl;
+    cout << "The values you chose make green" << endl;
 }
 else if (crb > 0 && crb <= 50 && cred < cgreen){
     cout << endl;
-    cout << "The color values make green-ish" << endl;
+    cout << "The values you chose make green-ish" << endl;
 }
 else if (cbr > 0 && cbr <= 50 && cblue < cgreen){
     cout << endl;
-    cout << "The color values make green-ish" << endl;
+    cout << "The values you chose make green-ish" << endl;
 }
 
 //If they make Blue
 if (cred == cgreen && cred < cblue){
     cout << endl;
-    cout << "The color values make blue" << endl;
+    cout << "The values you chose make blue" << endl;
 }
 else if (crg > 0 && crg <= 50 && cred < cblue){
     cout << endl;
-    cout << "The color values make blue-ish" << endl;
+    cout << "The values you chose make blue-ish" << endl;
 }
 else if (cgr > 0 && cgr <= 50 && cgreen < cblue){
     cout << endl;
-    cout << "The color values make blue-ish" << endl;
+    cout << "The values you chose make blue-ish" << endl;
+}
+
+//Vector Storage of 50 random values between 0 and 255
+do {
+  list.push_back(rand() % 256);
+} while (list.size() != 50);
+
+//Vector Storage for 5 red values
+do {
+  redlist.push_back(randomColor(list));
+} while (redlist.size() != 5);
+
+//Vector Storage for 5 green values
+do {
+  greenlist.push_back(randomColor(list));
+} while (greenlist.size() != 5);
+
+//Vector Storage for 5 blue values
+do {
+  bluelist.push_back(randomColor(list));
+} while (bluelist.size() != 5);
+
+cout << "\n" <<"Here are 5 other colors:" <<endl;
+for (int i = 0; i < bluelist.size(); i++){
+  cout << redlist[i] << ", " << greenlist[i] << ", " << bluelist[i] << "\n";
 }
 
 //Ask if it wants to be saved to a file
 
 cout << endl;
-cout << "Do you want to save to a File 'RGBvalues.md'? (Y or N)" << endl;
+cout << "Do you want to save the values and 5 other colors to 'RGBvalues.txt'? (Y or N)" << endl;
 cin >> letter;
 
 reader.open("RGBvalues.txt");
 
 while (letter != 'N' || letter != 'n'){
   if (letter == 'Y' || letter == 'y'){
-
+    reader << "The Values You Picked" << "\n";
     reader << "R: " << cred << "\n";
     reader << "G: " << cgreen << "\n";
     reader << "B: " << cblue << "\n";
+    reader << "\n" << "The 5 other Colors:" << "\n";
+    reader << redlist[0] << ", " << greenlist[0] << ", " << bluelist[0] << "\n";
+    reader << redlist[1] << ", " << greenlist[1] << ", " << bluelist[1] << "\n";
+    reader << redlist[2] << ", " << greenlist[2] << ", " << bluelist[2] << "\n";
+    reader << redlist[3] << ", " << greenlist[3] << ", " << bluelist[3] << "\n";
+    reader << redlist[4] << ", " << greenlist[4] << ", " << bluelist[4];
 
     cout << endl;
-    cout << "File Saved" << endl;
+    cout << "!File Saved!" << endl;
     cout << endl;
 
     reader.close();
@@ -170,7 +182,7 @@ while (letter != 'N' || letter != 'n'){
   }
 }
 
-cout << "File 'RGBvalues.txt' says:" << endl;
+cout << "File 'RGBvalues.txt' says:" << "\n" << endl;
 reader.open("RGBvalues.txt", ios::in);
 if (reader.is_open() ){
   while (!reader.eof()){
@@ -180,8 +192,30 @@ if (reader.is_open() ){
 reader.close();
 }
 else {
-  cout << "Could not open " << "RGBvalues.txt" << endl;
+  cout << "Could not open 'RGBvalues.txt' " << endl;
 }
 
   return 0;
+}
+
+//Function that checks if Red, Green, or Blue is between 0 and 255
+void checkValues(int & number) {
+  while (number <= 0 || number >= 0){
+  if (number < 0 || number > 255){
+    cout << endl;
+    cout << "ERROR: value not between 0-255" << endl;
+    cout << endl;
+    cout << "Enter a value between (0-255): ";
+    cin >> number;
+  } else {
+    break;
+  }
+  }
+}
+
+//Function that picks a random integer from a vector <int> storage
+int randomColor(vector <int> file){
+  int number = file[rand() % 50];
+
+  return number;
 }
